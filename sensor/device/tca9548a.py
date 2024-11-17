aaa0240e90d8ec3cdaad9aa74a2874540d3693f7
Thomas Herrmann <mail@thoherr.de>
2024-11-17 16:43:03 +0100

    reduce chatty code

--- sensor/device/tca9548a.py
+++ sensor/device/tca9548a.py
@@ -6,25 +6,9 @@ class TCA9548A:
         self._address = address
 
     def switch_to_channel(self, channel : int = -1):
-        mask = b'0x00'
-        # TODO/FIXME: This could be done way mor efficient with a shift
-        if channel == 0:
-            mask = b'0x01'
-        elif channel == 1:
-            mask = b'0x02'
-        elif channel == 2:
-            mask = b'0x04'
-        elif channel == 3:
-            mask = b'0x08'
-        elif channel == 4:
-            mask = b'0x10'
-        elif channel == 5:
-            mask = b'0x20'
-        elif channel == 6:
-            mask = b'0x40'
-        elif channel == 7:
-            mask = b'0x80'
+        if 0 <= channel <= 7:
+            mask = 0x01 << channel
         else:
-            mask = b'0x00'
+            mask = 0x00
 
-        self._i2c.writeto(self._address, mask)
+        self._i2c.writeto(self._address, mask.to_bytes(1))
