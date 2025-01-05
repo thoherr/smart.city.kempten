707caae900f8679a127171cfb3f4b4d0b4dcf836
Thomas Herrmann <mail@thoherr.de>
2025-01-05 22:26:19 +0100

    add light sensor class

---
+++ sensor/domain/environment/light.py
@@ -0,0 +1,9 @@
+# Sensor for the light level (in lux)
+
+class Light:
+    def __init__(self, location, sensor):
+        self.location = location
+        self._sensor = sensor
+
+    def light(self):
+        return self._sensor.value()
