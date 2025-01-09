dcd6a5c0262c517637ddd84ba4b0b375b8b1d2e5
Thomas Herrmann <mail@thoherr.de>
2025-01-09 22:29:30 +0100

    add experimental asyncio (to allow noise sensor to have one second sample interval and still have a responsive main loop)

--- sensor/device/KY037.py
+++ sensor/device/KY037.py
@@ -1,26 +1,30 @@
-import time
-
+import asyncio
 from machine import ADC
 
-from display.freesans20 import min_ch
-
-
+# HINT: The sensor has to be calibrated, so that the left LED is OFF in silence and
+#       begins to flicker at some noise. In order to pick up the noise, measurement
+#       runs in background at a time slice of about one second. Otherwise, the
+#       value is more or less random. The value range is quite small, e.g.
+#       between 48000 and 52000
 class KY037:
     def __init__(self, adc_id=0):
         self._adc = ADC(adc_id)
+        self._current_min_value = 0
+        self._current_max_value = 0
 
-    def value(self):
-        min_value = 65538
-        max_value = 0
-        for i in range(1000):
-            raw_value = self._adc.read_u16()
-            if raw_value > max_value:
-                max_value = raw_value
-            if raw_value < min_value:
-                min_value = raw_value
-            time.sleep(0.002)
-        volt = round((max_value - min_value) * 5 / 65536, 2)
-        return volt
+    async def read(self):
+        while True:
+            min_value = 65538
+            max_value = 0
+            for i in range(100):
+                raw_value = self._adc.read_u16()
+                if raw_value > max_value:
+                    max_value = raw_value
+                if raw_value < min_value:
+                    min_value = raw_value
+                await asyncio.sleep_ms(10)
+            self._current_min_value = min_value
+            self._current_max_value = max_value
 
     def noise(self):
-        return self.value()
+        return [self._current_min_value, self._current_max_value]
--- testboard.py
+++ testboard.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import micropython
 
 from sensor.domain.environment.light import Light
@@ -50,7 +52,6 @@ else:
 
 
 led = Pin("LED", Pin.OUT)
-reed = Pin(15, Pin.IN, Pin.PULL_DOWN)
 
 multiplexer = TCA9548A(i2c1)
 p0 = ParkingSpace(multiplexer, 0, VL53L0X)
@@ -60,7 +61,8 @@ parking = ParkingArea("Illerufer", [p0, p2, p0, p2, p0, p2, p0, p2, p0, p2, p0, 
 waste_container = WasteContainer("Müll 1", multiplexer, 6, GY302)
 light_sensor = Light("Fußgängerzone", multiplexer, 6, GY302)
 weather_sensor = Weather("Innenstadt",  multiplexer, 7, BME280)
-noise_sensor = Noise("Strassenlärm", KY037())
+ky037 = KY037()
+noise_sensor = Noise("Strassenlärm", ky037)
 
 screen1 = SSD1306_I2C(128, 32, i2c1)
 writer1 = Writer(screen1, display.freesans20)
@@ -72,56 +74,64 @@ writer3 = Writer(screen2, display.freesansbold40)
 traffic_pin = Pin(14, Pin.IN)
 traffic = TrafficCount("Rathausplatz", traffic_pin)
 
-while True:
-    traffic.check()
-    parking.check()
-    waste_container.check()
-    waste_status = "Waste {:s} {:s}".format(waste_container.location, "full" if waste_container.full() else "OK")
-    reed_value = "Reed value {:1d}".format(reed.value())
-    multiplexer.switch_to_channel(0)
-    traffic_count = "Traffic at {:s} {:1d}".format(traffic.location, traffic.get_count())
-    number_of_empty_spaces = parking.number_of_empty_spaces()
-    number_of_spaces = parking.number_of_spaces()
-    parking_lots_available = "{:1d}".format(number_of_empty_spaces)
-    parking_lots = "{:1d} / {:1d}".format(number_of_empty_spaces, number_of_spaces)
-    parking_status = "{:6s}".format("  FREI" if number_of_empty_spaces > 0 else "BELEGT")
+async def main_loop():
+    print("----- main_loop starting")
+    while True:
+        traffic.check()
+        parking.check()
+        waste_container.check()
+        waste_status = "Waste {:s} {:s}".format(waste_container.location, "full" if waste_container.full() else "OK")
+        multiplexer.switch_to_channel(0)
+        traffic_count = "Traffic at {:s} {:1d}".format(traffic.location, traffic.get_count())
+        number_of_empty_spaces = parking.number_of_empty_spaces()
+        number_of_spaces = parking.number_of_spaces()
+        parking_lots_available = "{:1d}".format(number_of_empty_spaces)
+        parking_lots = "{:1d} / {:1d}".format(number_of_empty_spaces, number_of_spaces)
+        parking_status = "{:6s}".format("  FREI" if number_of_empty_spaces > 0 else "BELEGT")
 
-    weather = "Weather at {:s}:\n  Temperature {:.2f} °C\n  Pressure {:.2f} hPa\n  Humidity {:.2f} %".format(weather_sensor.location, weather_sensor.temperature(), weather_sensor.pressure(), weather_sensor.humidity())
+        weather = "Weather at {:s}:\n  Temperature {:.2f} °C\n  Pressure {:.2f} hPa\n  Humidity {:.2f} %".format(weather_sensor.location, weather_sensor.temperature(), weather_sensor.pressure(), weather_sensor.humidity())
+        light = "Light at {:s} is {:.2f}".format(light_sensor.location, light_sensor.light())
+        noise = "Noise at {:s} is {}".format(noise_sensor.location, noise_sensor.noise())
 
-    light = "Light at {:s} is {:.2f}".format(light_sensor.location, light_sensor.light())
+        print(waste_status)
+        print(traffic_count)
+        print(parking_lots)
+        print(weather)
+        print(light)
+        print(noise)
 
-    noise = "Noise at {:s} is {:.2f}".format(noise_sensor.location, noise_sensor.noise())
+        await asyncio.sleep_ms(10)
 
-    print(waste_status)
-    print(reed_value)
-    print(traffic_count)
-    print(parking_lots)
-    print(weather)
-    print(light)
-    print(noise)
+        screen1.fill(0)
+        screen1.text("Parkplatz", 0, 0, 1)
+        screen1.text(parking.location, 0, 12, 1)
+        #screen1.text(parking_lots, 88, 0, 1)
+        if number_of_empty_spaces > 0:
+            writer1.set_textpos(screen1, 0, 128 - writer1.stringlen(parking_lots_available))
+            writer1.printstring(parking_lots_available)
+        screen1.text(parking_status, 80, 24, 1)
+        screen1.show()
 
-    screen1.fill(0)
-    screen1.text("Parkplatz", 0, 0, 1)
-    screen1.text(parking.location, 0, 12, 1)
-    #screen1.text(parking_lots, 88, 0, 1)
-    if number_of_empty_spaces > 0:
-        writer1.set_textpos(screen1, 0, 128 - writer1.stringlen(parking_lots_available))
-        writer1.printstring(parking_lots_available)
-    screen1.text(parking_status, 80, 24, 1)
-    screen1.show()
+        screen2.fill(0)
+        writer2.set_textpos(screen2,0, 0)
+        writer2.printstring("Parkplatz\n{:s}".format(parking.location))
+        #screen2.text(parking_lots, 88, 0, 1)
+        if number_of_empty_spaces > 0:
+            writer3.set_textpos(screen2, 0, 128 - writer3.stringlen(parking_lots_available))
+            writer3.printstring(parking_lots_available)
+        writer2.set_textpos(screen2, 44, 128 - writer2.stringlen(parking_status))
+        writer2.printstring(parking_status)
+        screen2.show()
 
-    screen2.fill(0)
-    writer2.set_textpos(screen2,0, 0)
-    writer2.printstring("Parkplatz\n{:s}".format(parking.location))
-    #screen2.text(parking_lots, 88, 0, 1)
-    if number_of_empty_spaces > 0:
-        writer3.set_textpos(screen2, 0, 128 - writer3.stringlen(parking_lots_available))
-        writer3.printstring(parking_lots_available)
-    writer2.set_textpos(screen2, 44, 128 - writer2.stringlen(parking_status))
-    writer2.printstring(parking_status)
-    screen2.show()
+        print(memory_usage.df())
+        print(memory_usage.free(True))
 
-    print(memory_usage.df())
-    print(memory_usage.free(True))
+        await asyncio.sleep_ms(500)
 
-    time.sleep(0.5)
+async def main():
+    print("##### main starting")
+    await asyncio.gather(asyncio.create_task(ky037.read()), asyncio.create_task(main_loop()))
+
+print("##### Testboard starting")
+asyncio.run(main())
+print("##### Testboard done")
