d3129b7b78af81647ec7145946d1fbcd399aad3c
Thomas Herrmann <mail@thoherr.de>
2025-01-11 18:44:10 +0100

    cleanup and format files

--- util/heartbeat.py
+++ util/heartbeat.py
@@ -1,10 +1,11 @@
+import asyncio
 import time
 
 from machine import Pin
-import asyncio
+
 
 class Heartbeat:
-    def __init__(self, pin = "LED", interval=1000, ratio=0.05, print_timestamp=False):
+    def __init__(self, pin="LED", interval=1000, ratio=0.05, print_timestamp=False):
         self._led = Pin(pin, Pin.OUT)
         self._interval = interval
         self._ratio = ratio
@@ -19,5 +20,7 @@ class Heartbeat:
             self._led.on()
             if self._print_timestamp:
                 now = time.gmtime()
-                print("{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d} heartbeat".format(now[0], now[1], now[2], now[3], now[4], now[5]))
+                print(
+                    "{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d} heartbeat".format(now[0], now[1], now[2], now[3], now[4],
+                                                                                now[5]))
             await asyncio.sleep_ms(self._on_time)
