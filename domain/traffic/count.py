34b0cf29d443f51d00afe0b82c2b50a3028b0e7c
Thomas Herrmann <mail@thoherr.de>
2025-01-16 00:06:59 +0100

    convert traffic count to actor

--- domain/traffic/count.py
+++ domain/traffic/count.py
@@ -1,22 +1,20 @@
 # Sensor for traffic count, using a PIO (e.g. reed or hall sensor)
-import asyncio
+
+from util.actor import Actor
 
 
-class TrafficCount:
-    def __init__(self, location : str, binary_sensor, interval=0.25):
-        self._counter : int = 0
-        self.location = location
+class TrafficCount(Actor):
+    def __init__(self, location: str, binary_sensor, interval=0.2, verbose=False):
+        super().__init__(location, interval, verbose)
+        self._counter: int = 0
         self._binary_sensor = binary_sensor
-        self._interval = interval
         self._old_value = self._binary_sensor.value()
 
-    async def run(self):
-        while True:
-            new_value = self._binary_sensor.value()
-            if new_value != self._old_value:
-                self._old_value = new_value
-                self._counter += self._old_value == 1 # only count rising value
-            await asyncio.sleep(self._interval)
+    async def work(self):
+        new_value = self._binary_sensor.value()
+        if new_value != self._old_value:
+            self._counter += (new_value == 1)  # only count rising value
+            self._old_value = new_value
 
     def get_count(self):
         return self._counter
