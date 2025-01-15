d7580f9a361eb1df4206c75561b59bbf6e8d4a51
Thomas Herrmann <mail@thoherr.de>
2025-01-15 23:40:47 +0100

    add waste containers to inner city; convert (waste) container class to Actor

--- domain/waste/container.py
+++ domain/waste/container.py
@@ -1,9 +1,11 @@
 # Sensor for a (single) waste container, using a light barrier
-import asyncio
+from util.actor import Actor
 
 
-class WasteContainer:
-    def __init__(self, location : str, multiplexer, channel, sensor_class, threshold=100, interval=1):
+class WasteContainer(Actor):
+    def __init__(self, waste_container_id, location: str, multiplexer, channel, sensor_class, threshold=100, interval=1,
+                 verbose=False):
+        super().__init__("WasteContainer {:s}".format(waste_container_id), interval, verbose)
         self.location = location
         self._multiplexer = multiplexer
         self._channel = channel
@@ -13,12 +15,7 @@ class WasteContainer:
         self._interval = interval
         self._is_full = False
 
-    async def run(self):
-        while True:
-            self._check()
-            await asyncio.sleep(self._interval)
-
-    def _check(self):
+    async def work(self):
         self._multiplexer.switch_to_channel(self._channel)
         self._is_full = self._sensor.value() < self._threshold
 
--- smart_city/controller_inner_city.py
+++ smart_city/controller_inner_city.py
@@ -5,9 +5,11 @@
 import asyncio
 
 from device.multiplexer.tca9548a import TCA9548A
+from device.sensor.gy302 import GY302
 from device.sensor.vl53l0x import VL53L0X
 from domain.parking.area import ParkingArea
 from domain.parking.space import ParkingSpace
+from domain.waste.container import WasteContainer
 from report.parking import ParkingAreaPanelSH1106
 from smart_city.controller_base import ControllerBase
 from util.heartbeat import Heartbeat
@@ -19,24 +21,42 @@ class ControllerInnerCity(ControllerBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._location = "Innenstadt"
-        self.multiplexer = TCA9548A(self.i2c1)
-        p1 = ParkingSpace("Rathaus 1", self.multiplexer, 0, VL53L0X, empty_threshold=50, verbose=True)
-        p2 = ParkingSpace("Rathaus 2", self.multiplexer, 1, VL53L0X, empty_threshold=100, verbose=True)
-        p3 = ParkingSpace("Rathaus 3", self.multiplexer, 2, VL53L0X, empty_threshold=65, verbose=True)
-        p4 = ParkingSpace("Rathaus 4", self.multiplexer, 3, VL53L0X, empty_threshold=50, verbose=True)
-        p5 = ParkingSpace("Rathaus 5", self.multiplexer, 4, VL53L0X, empty_threshold=50, verbose=True)
-        p6 = ParkingSpace("Rathaus 6", self.multiplexer, 5, VL53L0X, empty_threshold=40, verbose=True)
+
+        self.actors = []
+
+        self.actors.append(Heartbeat(print_timestamp=True))
+        self.actors.append(Housekeeper(verbose=True))
+
+        self.mux1 = TCA9548A(self.i2c1, address=0x70)
+        p1 = ParkingSpace("Rathaus 1", self.mux1, 0, VL53L0X, empty_threshold=50, verbose=True)
+        p2 = ParkingSpace("Rathaus 2", self.mux1, 1, VL53L0X, empty_threshold=100, verbose=True)
+        p3 = ParkingSpace("Rathaus 3", self.mux1, 2, VL53L0X, empty_threshold=65, verbose=True)
+        p4 = ParkingSpace("Rathaus 4", self.mux1, 3, VL53L0X, empty_threshold=50, verbose=True)
+        p5 = ParkingSpace("Rathaus 5", self.mux1, 4, VL53L0X, empty_threshold=50, verbose=True)
+        p6 = ParkingSpace("Rathaus 6", self.mux1, 5, VL53L0X, empty_threshold=40, verbose=True)
         self.parking = ParkingArea("Rathaus", [p1, p2, p3, p4, p5, p6])
+        self.actors.append(self.parking)
+
         self.parking_panel_large = ParkingAreaPanelSH1106(self.i2c0, self.parking, verbose=True)
+        self.actors.append(self.parking_panel_large)
+
+        self.mux2 = TCA9548A(self.i2c1, address=0x71)
+
+        w1 = WasteContainer("Müll 1", "Rathaus", self.mux2, 6, GY302)
+        self.actors.append(w1)
+        w2 = WasteContainer("Müll 2", "Rathaus", self.mux2, 6, GY302)
+        self.actors.append(w2)
+        w3 = WasteContainer("Müll 3", "Rathaus", self.mux2, 6, GY302)
+        self.actors.append(w3)
+        self.waste = [w1, w2, w3]
 
     def print_debug_log(self):
         number_of_empty_spaces = self.parking.number_of_empty_spaces()
         number_of_spaces = self.parking.number_of_spaces()
         parking_lots = "{:1d} / {:1d}".format(number_of_empty_spaces, number_of_spaces)
         print(parking_lots)
+        for waste in self.waste:
+            print("Waste {:s} {:s}".format(waste.location, "full" if waste.full() else "OK"))
 
     async def create_tasks(self):
-        return asyncio.gather(asyncio.create_task(Heartbeat(print_timestamp=True).run()),
-                              asyncio.create_task(Housekeeper(verbose=True).run()),
-                              asyncio.create_task(self.parking.run()),
-                              asyncio.create_task(self.parking_panel_large.run()))
+        return asyncio.gather(self.actors)
