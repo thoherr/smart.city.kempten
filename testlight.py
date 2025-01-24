067ff2d38c68767ef5c3064fbd734ea47affb51b
Thomas Herrmann <mail@thoherr.de>
2025-01-24 17:22:54 +0100

    use Actors for test routine

--- testlight.py
+++ testlight.py
@@ -1,24 +1,26 @@
 import micropython
+import asyncio
 
 micropython.alloc_emergency_exception_buf(100)
 
 from machine import Pin
-import time
 
-from domain.traffic.light import Column as TrafficLightColumn
-from domain.traffic.light import Crossing as TrafficLightCrossing
+from domain.traffic.light.column import Column as TrafficLightColumn
+from domain.traffic.light.crossing import Crossing as TrafficLightCrossing
+from util.heartbeat import Heartbeat
+from util.housekeeper import Housekeeper
 
 led = Pin("LED", Pin.OUT)
 
 l1 = TrafficLightColumn(9, 10, 11, 1)
 l2 = TrafficLightColumn(14, 13, 12, 0)
 
-crossing = TrafficLightCrossing([l1, l2])
+crossing = TrafficLightCrossing("Testcrossing", [l1, l2])
 
-while True:
-    for i in range(100):
-        led.toggle()
-        crossing.next_tick()
+async def main_loop():
+    print("----- main_loop starting")
+    i = 0
+    while True:
         if i == 65:
             crossing.turn_on(TrafficLightCrossing.OUT_OF_ORDER_CIRCUIT)
         elif i == 75:
@@ -27,4 +29,14 @@ while True:
             crossing.turn_on(TrafficLightCrossing.OUT_OF_ORDER_CIRCUIT)
         elif i == 99:
             crossing.turn_on()
-        time.sleep(1)
+            i = 0
+
+async def main():
+    print("##### main starting")
+    await asyncio.gather(asyncio.create_task(crossing.run()),
+                         asyncio.create_task(Heartbeat(verbose=True).run()),
+                         asyncio.create_task(Housekeeper(verbose=True).run()))
+
+print("##### Testlight starting")
+asyncio.run(main())
+print("##### Testlight done")
