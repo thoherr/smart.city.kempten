f878f639a6094ce5a6a73a34a5c868bc9e0537f1
Thomas Herrmann <mail@thoherr.de>
2025-01-25 18:22:39 +0100

    clarify comment

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@
 # This is the configuration file to define the role of the raspi
 # by defining the specific instance of a SmartCityController
 
-# the file setup_controller_number.py should contain the number of our controller/board, e.g.
-# controller_number = 1
+# The file setup_controller_number.py should contain the number of our controller/board, e.g.
+#
+#   # This specifies on which controller (PICO board) we are running
+#   controller_number = 1
+#
 import setup_controller_number
 
 if setup_controller_number.controller_number == 1:
