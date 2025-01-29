74bbd34d44f9d983cd14becc49aa54ec484ee4fb
Thomas Herrmann <mail@thoherr.de>
2025-01-29 16:42:54 +0100

    skeleton for data upload

--- report/dashboard_upload.py
+++ report/dashboard_upload.py
@@ -6,7 +6,15 @@ class DashboardUpload(Actor):
         super().__init__(actor_id, interval=interval, verbose=verbose)
         self._dashboard = dashboard
         self._sensor = sensor
+        self._current_value = None
 
     async def work(self):
+        new_value = self._sensor.value()
+        if new_value != self._current_value:
+            self._current_value = new_value
+            self.upload_data()
         if self._verbose:
             self.log("Uploading dashboard data for sensor %s" % self._sensor)
+
+    def upload_data(self):
+        pass
