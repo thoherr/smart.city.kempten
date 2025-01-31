a9e8d1af04e4e6d69bb710b4b0c67b198c4b00cb
Thomas Herrmann <mail@thoherr.de>
2025-01-31 16:35:44 +0100

    include MQTT client in dashboard upload (skeleton)

--- report/dashboard_upload.py
+++ report/dashboard_upload.py
@@ -1,20 +1,25 @@
-from util.actor import Actor
+import json
 
+from util.actor import Actor
+from report.umqtt.robust import MQTTClient
 
 class DashboardUpload(Actor):
-    def __init__(self, actor_id, dashboard, sensor, interval=2, verbose=False):
+    def __init__(self, actor_id, mqtt_client : MQTTClient, sensor, interval=2, verbose=False):
         super().__init__(actor_id, interval=interval, verbose=verbose)
-        self._dashboard = dashboard
+        self._mqtt_client = mqtt_client
         self._sensor = sensor
         self._current_value = None
+        self._value_published = False
 
     async def work(self):
         new_value = self._sensor.value()
-        if new_value != self._current_value:
+        if new_value != self._current_value or not self._value_published:
             self._current_value = new_value
-            self.upload_data()
+            self.publish_value()
+
+    def publish_value(self):
         if self._verbose:
             self.log("Uploading dashboard data for sensor %s" % self._sensor)
-
-    def upload_data(self):
-        pass
+        topic = self.actor_id
+        msg = json.dumps({"self._current_value": self._current_value})
+        self._value_published = self._mqtt_client.publish(topic, msg, retain=False, qos=0)
