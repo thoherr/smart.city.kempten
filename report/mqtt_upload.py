6fc222ce98fc4d1b8bdd5836931209dc7f542dee
Thomas Herrmann <mail@thoherr.de>
2025-02-09 17:07:59 +0100

    Refactor MqttUpload (split upload and actor functionality)

--- report/mqtt_upload.py
+++ report/mqtt_upload.py
@@ -6,12 +6,27 @@ import setup_mqtt_config as mqtt_config
 from report.umqtt.robust import MQTTClient
 from util.actor import Actor
 
+class MqttUpload:
 
-class MqttUploadActor(Actor):
-    def __init__(self, actor_id, mqtt_client: MQTTClient, value_method, interval=2, verbose=False):
-        super().__init__(actor_id, interval=interval, verbose=verbose)
+    def __init__(self, mqtt_topic, mqtt_client: MQTTClient, verbose=False):
+        self._mqtt_topic = mqtt_topic
+        self._verbose = verbose
+        self._topic = f"{mqtt_config.mqtt_topic_root}/{self._mqtt_topic}"
         self._mqtt_client = mqtt_client
-        self._topic = f"{mqtt_config.mqtt_topic_root}/{self.actor_id}"
+
+    def post_data(self, data):
+        now = utime.gmtime()
+        timestamp = f"{now[0]:4d}-{now[1]:02d}-{now[2]:02d}T{now[3]:02d}:{now[4]:02d}:{now[5]:02d}Z"
+        msg = json.dumps({"id": self._mqtt_topic,
+                          "timestamp": timestamp,
+                          "payload": data})
+        self._mqtt_client.publish(self._topic, msg, retain=False, qos=0)
+
+
+class MqttUploadActor(MqttUpload, Actor):
+    def __init__(self, mqtt_topic, mqtt_client: MQTTClient, value_method, interval=2, verbose=False):
+        MqttUpload.__init__(self, mqtt_topic, mqtt_client, verbose=False)
+        Actor.__init__(self, mqtt_topic, interval=interval, verbose=verbose)
         self._value_method = value_method
         self._current_value = None
 
@@ -21,14 +36,6 @@ class MqttUploadActor(Actor):
         new_value = self._value_method()
         if new_value != self._current_value:
             self._current_value = new_value
-            self.post_data()
-
-    def post_data(self):
-        if self._verbose:
-            self.log(f"Uploading changed data for {self.actor_id} to MQTT")
-        now = utime.gmtime()
-        timestamp = f"{now[0]:4d}-{now[1]:02d}-{now[2]:02d}T{now[3]:02d}:{now[4]:02d}:{now[5]:02d}Z"
-        msg = json.dumps({"id": self.actor_id,
-                          "timestamp": timestamp,
-                          "payload": self._current_value})
-        self._mqtt_client.publish(self._topic, msg, retain=False, qos=0)
+            if self._verbose:
+                self.log(f"Uploading changed data for {self.actor_id} to MQTT")
+            self.post_data(new_value)
