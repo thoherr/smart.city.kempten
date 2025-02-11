5d654f9d986b7a97b743b728f5cc06ee662025ae
Thomas Herrmann <mail@thoherr.de>
2025-02-11 17:30:52 +0100

    add traffic counter

--- smart_city/controller_iller.py
+++ smart_city/controller_iller.py
@@ -1,6 +1,7 @@
 # This is the third Raspi of our MOC of the City of Kempten
 # It implements the Illerufer (second baseplate), nearby the river, with a Parking Area (including display),
 # three waste containers and one traffic light
+from machine import Pin
 
 from device.driver.gy302 import GY302
 from device.driver.tca9548a import TCA9548A
@@ -8,9 +9,10 @@ from device.driver.vl53l0x import VL53L0X
 from device.multiplexed_i2c_sensor import MultiplexedI2cSensor
 from domain.parking.area import ParkingArea
 from domain.parking.space import ParkingSpace
+from domain.traffic.count import TrafficCount
 from domain.waste.area import WasteArea
 from domain.waste.container import WasteContainer
-from report.mqtt_upload import MqttUploadActor
+from report.mqtt_upload import MqttUploadActor, MqttUpload
 from report.parking_area_panel_sh1106 import ParkingAreaPanelSH1106
 from smart_city.controller_base import ControllerBase
 
@@ -23,6 +25,7 @@ class ControllerIller(ControllerBase):
 
         self._init_parking()
         self._init_waste()
+        self._init_traffic()
 
 #        self.parking_panel_large = ParkingAreaPanelSH1106(self.i2c0, self.parking, self.waste, verbose=False)
 #        self.actors.append(self.parking_panel_large)
@@ -73,3 +76,20 @@ class ControllerIller(ControllerBase):
         self.parking_upload = MqttUploadActor("parkraum/sck_parkraum_2", self.mqtt_client, self.parking.status, interval=3,
                                               verbose=True)
         self.actors.append(self.parking_upload)
+
+    def _init_traffic(self):
+        mqtt_traffic_4 = MqttUpload("verkehr/sck_verkehr_4", self.mqtt_client, qos=1, verbose=True)
+
+        in_traffic_4 = TrafficCount("RH 4 einwärts", "eingehend", Pin(26, Pin.IN), mqtt_traffic_4, verbose=True)
+        self.actors.append(in_traffic_4)
+        out_traffic_4 = TrafficCount("RH 4 auswärts", "ausgehend", Pin(27, Pin.IN), mqtt_traffic_4, verbose=True)
+        self.actors.append(out_traffic_4)
+
+#        counters_4 = [out_traffic_4, in_traffic_4]
+#        traffic_count_panel = TrafficCountPanel("traffic", self.i2c0, counters_4, verbose=True)
+#        self.actors.append(traffic_count_panel)
+
+        mqtt_traffic_5 = MqttUpload("verkehr/sck_verkehr_5", self.mqtt_client, verbose=True)
+
+        traffic_5 = TrafficCount("RH 5", "durchfahrt", Pin(28, Pin.IN), mqtt_traffic_5, verbose=True)
+        self.actors.append(traffic_5)
