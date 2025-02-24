2ee861424a8dc374506c2adda772b355b9b702d9
Thomas Herrmann <mail@thoherr.de>
2025-02-24 18:36:16 +0100

    make park and waste sensor verbosity easily configurable for calibration purposes

--- smart_city/controller_inner_city_one.py
+++ smart_city/controller_inner_city_one.py
@@ -31,29 +31,29 @@ class ControllerInnerCityOne(ControllerBase):
         self.mux1 = TCA9548A(self.i2c1, address=0x70)
         p1 = ParkingSpace("Rathaus 1", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux1, channel=5),
                           interval=PARKING_SPACE_CHECK_INTERVAL,
-                          empty_threshold=40)
+                          empty_threshold=40, verbose=PARK_SENSOR_VERBOSITY)
         p2 = ParkingSpace("Rathaus 2", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux1, channel=4),
                           interval=PARKING_SPACE_CHECK_INTERVAL,
-                          empty_threshold=50)
+                          empty_threshold=50, verbose=PARK_SENSOR_VERBOSITY)
         p3 = ParkingSpace("Rathaus 3", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux1, channel=3),
                           interval=PARKING_SPACE_CHECK_INTERVAL,
-                          empty_threshold=52)
+                          empty_threshold=52, verbose=PARK_SENSOR_VERBOSITY)
         p4 = ParkingSpace("Rathaus 4", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux1, channel=2),
                           interval=PARKING_SPACE_CHECK_INTERVAL,
-                          empty_threshold=60)
+                          empty_threshold=60, verbose=PARK_SENSOR_VERBOSITY)
         p5 = ParkingSpace("Rathaus 5", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux1, channel=1),
                           interval=PARKING_SPACE_CHECK_INTERVAL,
-                          empty_threshold=95)
+                          empty_threshold=95, verbose=PARK_SENSOR_VERBOSITY)
         p6 = ParkingSpace("Rathaus 6", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux1, channel=0),
-                          empty_threshold=40)
+                          empty_threshold=40, verbose=PARK_SENSOR_VERBOSITY)
 
         self.mux2 = TCA9548A(self.i2c1, address=0x71)
         p7 = ParkingSpace("Rathaus 7", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux2, channel=7),
                           interval=PARKING_SPACE_CHECK_INTERVAL,
-                          empty_threshold=70)
+                          empty_threshold=70, verbose=PARK_SENSOR_VERBOSITY)
         p8 = ParkingSpace("Rathaus 8", MultiplexedI2cSensor(VL53L0X, multiplexer=self.mux2, channel=1),
                           interval=PARKING_SPACE_CHECK_INTERVAL,
-                          empty_threshold=53)
+                          empty_threshold=53, verbose=PARK_SENSOR_VERBOSITY)
 
         gc.collect()
 
@@ -69,13 +69,19 @@ class ControllerInnerCityOne(ControllerBase):
         gc.collect()
 
         w1 = WasteContainer("Rathaus 1",
-                            MultiplexedI2cSensor(GY302, multiplexer=self.mux3, channel=0), interval=SMART_WASTE_CHECK_INTERVAL)
+                            MultiplexedI2cSensor(GY302, multiplexer=self.mux3, channel=0),
+                            interval=SMART_WASTE_CHECK_INTERVAL,
+                            verbose=WASTE_SENSOR_VERBOSITY)
         self.actors.append(w1)
         w2 = WasteContainer("Rathaus 2",
-                            MultiplexedI2cSensor(GY302, multiplexer=self.mux3, channel=1), interval=SMART_WASTE_CHECK_INTERVAL)
+                            MultiplexedI2cSensor(GY302, multiplexer=self.mux3, channel=1),
+                            interval=SMART_WASTE_CHECK_INTERVAL,
+                            verbose=WASTE_SENSOR_VERBOSITY)
         self.actors.append(w2)
         w3 = WasteContainer("Rathaus 3",
-                            MultiplexedI2cSensor(GY302, multiplexer=self.mux3, channel=2), interval=SMART_WASTE_CHECK_INTERVAL)
+                            MultiplexedI2cSensor(GY302, multiplexer=self.mux3, channel=2),
+                            interval=SMART_WASTE_CHECK_INTERVAL,
+                            verbose=WASTE_SENSOR_VERBOSITY)
         self.actors.append(w3)
         self.waste = WasteArea("Rathaus", [w1, w2, w3])
 
