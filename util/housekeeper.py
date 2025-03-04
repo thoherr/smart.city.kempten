6e29646c3b913a5787e4e72b416ee77838d42e9a
Thomas Herrmann <mail@thoherr.de>
2025-03-04 12:01:52 +0100

    add debug flag

--- util/housekeeper.py
+++ util/housekeeper.py
@@ -5,19 +5,20 @@ from util.actor import Actor
 
 
 class Housekeeper(Actor):
-    def __init__(self, actor_id="Housekeeper", interval=1, verbose=False):
+    def __init__(self, actor_id="Housekeeper", interval=1, verbose=True, debug=False):
         super().__init__(actor_id, interval, verbose)
         self._interval = interval
         self._verbose = verbose
+        self._debug = debug
         gc.enable()
         self.mem_free, self.mem_alloc, self.mem_total, self.percentage_free = memory_usage.memory()
 
     async def work(self):
-        if self._verbose:
+        if self._debug:
             self.log(memory_usage.free(True))
         gc.collect()
         self.mem_free, self.mem_alloc, self.mem_total, self.percentage_free = memory_usage.memory()
-        if self._verbose:
+        if self._debug:
             self.log(memory_usage.free(True))
 
     def status(self):
